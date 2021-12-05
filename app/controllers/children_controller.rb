a9fe590491af9abcc39ab7b87847bb0e6ef87232
Maykon Franchini <maykonfranchini2017@gmail.com>
2021-12-05 09:55:18 +0000

    Balance and transactions table created

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -3,5 +3,16 @@ class ChildrenController < ApplicationController
 
   def dashboard
     @child = Child.find(current_child.id)
+    @transactions = Transaction.where(child_id: current_child.id)
+
+    balance = 0
+    @transactions.each do |transaction|
+      if transaction.transaction_type == "deposit"
+        balance = balance + transaction.amount
+      else
+        balance = balance - transaction.amount
+      end
+    end
+    @balance = balance
   end
 end
