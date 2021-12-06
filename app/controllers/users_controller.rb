9fa00c0228cbcc46743bf836d8c46220bad2cdef
Maykon Franchini <maykonfranchini2017@gmail.com>
2021-12-06 05:14:32 +0000

    Balance update  / transactions list styled

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
     before_action :authenticate_user!
 
   def dashboard
-    @transaction = Transaction.new
-  @user = User.find(current_user.id)
+    @user = User.find(current_user.id)
   end
 end
