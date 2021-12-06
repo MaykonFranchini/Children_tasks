80c2cb29d06147d358f100346d8143196be36795
Maykon Franchini <maykonfranchini2017@gmail.com>
2021-12-06 02:27:46 +0000

    Account table with references created

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -3,16 +3,6 @@ class ChildrenController < ApplicationController
 
   def dashboard
     @child = Child.find(current_child.id)
-    @transactions = Transaction.where(child_id: current_child.id)
-
-    balance = 0
-    @transactions.each do |transaction|
-      if transaction.transaction_type == "deposit"
-        balance = balance + transaction.amount
-      else
-        balance = balance - transaction.amount
-      end
-    end
-    @balance = balance
+    # @account = Account.find(child_id: current_child.id) || Account.create(@child)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_06_015052) do
+ActiveRecord::Schema.define(version: 2021_12_06_015220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2021_12_06_015052) do
     t.integer "amount"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "account_id", null: false
+    t.index ["account_id"], name: "index_transactions_on_account_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -65,4 +67,5 @@ ActiveRecord::Schema.define(version: 2021_12_06_015052) do
 
   add_foreign_key "accounts", "children"
   add_foreign_key "children", "users"
+  add_foreign_key "transactions", "accounts"
 end
