fc16d4229479c64fa031839ccc0a1a13e355e9df
Maykon Franchini <maykonfranchini2017@gmail.com>
2022-01-14 06:02:55 +0000

    Notifications controller created + show notifications on modal

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -5,6 +5,7 @@ class ChildrenController < ApplicationController
     @child = Child.find(current_child.id)
     @pendingTasks = Task.where(child_id: current_child.id, status: 'pending')
     # @account = Account.find(child_id: current_child.id) || Account.create(@child)
+    @notifications = Notification.where(child_id: @child)
   end
 
   def show
