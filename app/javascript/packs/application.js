4b02ba1710e6d5ab7d8f412edf20123a02040854
Maykon Franchini <maykonfranchini2017@gmail.com>
2022-02-03 07:48:40 +0000

    PWA config done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -31,6 +31,12 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   // transactionFormDisplay();
+
+   navigator.serviceWorker.register('/service-worker.js')
+    .then(registration => {
+      console.log('ServiceWorker registered: ', registration)
+    });
+
   tabs();
   taskUpdate();
 });
